47561195e2f907a68943db6d11d0ce3c61bd8f16
Zener_BLITZ <93589138+SchottkyDi0de@users.noreply.github.com>
2022-07-02 00:57:43 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 WG_APP_ID = os.getenv('WG_APP_ID')
+
 bot_settings = {
 'TOKEN':os.getenv('DISCORD_TOKEN'),
 'name':'[HVOLT] WoTB Statistics',
